816e8855fbf4f6ecd522acc025d2b51c3ff0a348
egevtech <egevtech@gmail.com>
2026-02-28 11:09:21 +0300

    Added custom stl path

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <threads.h>
+
+#ifndef BFSTD
+#define BFSTD "/usr/local/lib/libbfstd.a"
+#endif
 
 // TODO: Move Token operations and vector from this file
 typedef enum {
@@ -17,44 +21,44 @@ typedef enum {
   PRINT_NUMERICAL_LN,
 } Token;
 
-#define RET_TOKEN(token_name) \
-  result = token_name;        \
-  return &result; \
+#define RET_TOKEN(token_name)                                                  \
+  result = token_name;                                                         \
+  return &result;                                                              \
   break
 
 Token *parse_token(int sym) {
   static Token result;
 
   switch (sym) {
-    case '+':
-      RET_TOKEN(PLUS);
+  case '+':
+    RET_TOKEN(PLUS);
 
-    case '-':
-      RET_TOKEN(MINUS);
+  case '-':
+    RET_TOKEN(MINUS);
 
-    case '>':
-      RET_TOKEN(NEXT);
+  case '>':
+    RET_TOKEN(NEXT);
 
-    case '<':
-      RET_TOKEN(PREV);
+  case '<':
+    RET_TOKEN(PREV);
 
-    case '.':
-      RET_TOKEN(PRINT);
+  case '.':
+    RET_TOKEN(PRINT);
 
-    case 'p':
-      RET_TOKEN(PRINT_NUMERICAL);
+  case 'p':
+    RET_TOKEN(PRINT_NUMERICAL);
 
-    case 'P':
-      RET_TOKEN(PRINT_NUMERICAL_LN);
+  case 'P':
+    RET_TOKEN(PRINT_NUMERICAL_LN);
 
-    default:
-      printf("... nothing?\nUnexpected token: '%c'\n", sym);
-      return NULL;
+  default:
+    printf("... nothing?\nUnexpected token: '%c'\n", sym);
+    return NULL;
   }
 }
 
 typedef struct {
-  Token* data;
+  Token *data;
 
   int len;
   int capacity;
@@ -64,28 +68,28 @@ typedef struct {
 
 TokenVector make_vector(void) {
   const TokenVector tv = {
-    .data = malloc(sizeof(Token)),
-    .len = 0,
-    .capacity = sizeof(Token),
-    .element_size = sizeof(Token),
+      .data = malloc(sizeof(Token)),
+      .len = 0,
+      .capacity = sizeof(Token),
+      .element_size = sizeof(Token),
   };
 
   return tv;
 }
 
-void close_vector(TokenVector* vec)
-{
+void close_vector(TokenVector *vec) {
   free(vec->data);
   vec->len = 0;
   vec->capacity = 0;
 }
 
 // TODO: if error, return nullptr, don't escape program here
-void vector_push(TokenVector* vec, const Token token)
-{
+void vector_push(TokenVector *vec, const Token token) {
 
-  // printf("%d * %d (%d) = %d (needs %d)\n", vec->len, vec->element_size, vec->len * vec->element_size, vec->capacity, (vec->len + 1) * vec->element_size);
-  if ( vec->capacity < vec->len * vec->element_size ) {
+  // printf("%d * %d (%d) = %d (needs %d)\n", vec->len, vec->element_size,
+  // vec->len * vec->element_size, vec->capacity, (vec->len + 1) *
+  // vec->element_size);
+  if (vec->capacity < vec->len * vec->element_size) {
     printf("Abnormally vector size!\n");
 
     close_vector(vec);
@@ -94,7 +98,7 @@ void vector_push(TokenVector* vec, const Token token)
 
   vec->len++;
 
-  if ( vec->capacity < vec->len * vec->element_size ) {
+  if (vec->capacity < vec->len * vec->element_size) {
     vec->data = reallocarray(vec->data, vec->len, vec->element_size);
     vec->capacity = vec->len * vec->element_size;
   }
@@ -102,8 +106,8 @@ void vector_push(TokenVector* vec, const Token token)
   vec->data[vec->len - 1] = token;
 }
 
-Token *vector_pop(TokenVector* vec) {
-  if ( vec->len == 0 ) {
+Token *vector_pop(TokenVector *vec) {
+  if (vec->len == 0) {
     printf("Trying to pop an empty vector!\n");
     return NULL;
   }
@@ -114,8 +118,7 @@ Token *vector_pop(TokenVector* vec) {
   return &vec->data[vec->len - 1];
 }
 
-int main(const int argc, char **argv)
-{
+int main(const int argc, char **argv) {
   printf("BFC - brainfuck compiler\n");
 
   if (argc != 2) {
@@ -157,12 +160,12 @@ int main(const int argc, char **argv)
 
   fclose(file);
 
-  void* output_file = malloc(sizeof(char) * (strlen(argv[1]) + 2));
+  void *output_file = malloc(sizeof(char) * (strlen(argv[1]) + 2));
   strcpy(output_file, argv[1]);
   strcat(output_file, ".s");
 
-  printf("Opening output file %s\n", (char*)output_file);
-  FILE* output = fopen(output_file, "w");
+  printf("Opening output file %s\n", (char *)output_file);
+  FILE *output = fopen(output_file, "w");
 
   if (output == NULL) {
     printf("Can't open output file\n");
@@ -170,53 +173,54 @@ int main(const int argc, char **argv)
     return EXIT_FAILURE;
   }
 
-  const char* externs = "vector_init, next_cell, prev_cell, add_cell, sub_cell, print_cell, print_num, print_num_ln";
+  const char *externs = "vector_init, next_cell, prev_cell, add_cell, "
+                        "sub_cell, print_cell, print_num, print_num_ln";
 
-  fprintf(output, "section .data\nglobal _start\n\nvec dq 0\n"
-    "cell dd 0\n\nextern %s\n\nsection .text\n_start:\n\tcall vector_init\n"
-    "\tmov [vec], rax\n\n", externs);
+  fprintf(
+      output,
+      "section .data\nglobal _start\n\nvec dq 0\n"
+      "cell dd 0\n\nextern %s\n\nsection .text\n_start:\n\tcall vector_init\n"
+      "\tmov [vec], rax\n\n",
+      externs);
 
-  char* operation = malloc(20 * sizeof(char));
+  char *operation = malloc(20 * sizeof(char));
 
-  for ( int i = 0; i < tokens.len; i++ )
-  {
+  for (int i = 0; i < tokens.len; i++) {
     int pr = ((float)i + 1) / tokens.len * 100.0f;
 
     printf("Compiling... %d%%\r", pr);
 
-    switch ( tokens.data[i] )
-    {
+    switch (tokens.data[i]) {
     case PLUS:
-      strcpy(operation,"add_cell");
+      strcpy(operation, "add_cell");
       break;
     case MINUS:
-      strcpy(operation,"sub_cell");
+      strcpy(operation, "sub_cell");
       break;
 
     case NEXT:
-      strcpy(operation,"next_cell");
+      strcpy(operation, "next_cell");
       break;
     case PREV:
-      strcpy(operation,"prev_cell");
+      strcpy(operation, "prev_cell");
       break;
 
     case PRINT:
-      strcpy(operation,"print_cell");
+      strcpy(operation, "print_cell");
       break;
     case PRINT_NUMERICAL:
-      strcpy(operation,"print_num");
+      strcpy(operation, "print_num");
       break;
     case PRINT_NUMERICAL_LN:
-      strcpy(operation,"print_num_ln");
+      strcpy(operation, "print_num_ln");
       break;
     }
 
-    fprintf(output, "\tmov rdi, [vec]\n\tmov rsi, cell\n\tcall %s\n\n", operation);
-
+    fprintf(output, "\tmov rdi, [vec]\n\tmov rsi, cell\n\tcall %s\n\n",
+            operation);
   }
 
   free(operation);
-
 
   fprintf(output, "\tmov rax, 60\n\tmov rdi, 0\n\tsyscall");
   fclose(output);
@@ -226,18 +230,21 @@ int main(const int argc, char **argv)
   printf("Compiling... done\n");
 
   const char *compile_cmd_pattern = "nasm -felf64 %s.s -o %s.o";
-  const char *link_cmd_pattern = "ld %s.o build/libstdbf.a -o a.out -lc -dynamic-linker /lib64/ld-linux-x86-64.so.2";
+  const char *link_cmd_pattern = "ld %s.o %s -o a.out -lc "
+                                 "-dynamic-linker /lib64/ld-linux-x86-64.so.2";
 
-  char* compile_cmd = malloc(strlen(compile_cmd_pattern) + strlen(argv[1]) * 2 + 1);
-  char* link_cmd = malloc(strlen(link_cmd_pattern) + strlen(argv[1]) * 2 + 1);
+  char *compile_cmd =
+      malloc(strlen(compile_cmd_pattern) + strlen(argv[1]) * 2 + 1);
+  char *link_cmd = malloc(strlen(link_cmd_pattern) + strlen(argv[1]) * 2 + 1 +
+                          strlen(BFSTD));
 
   sprintf(compile_cmd, compile_cmd_pattern, argv[1], argv[1]);
-  sprintf(link_cmd, link_cmd_pattern, argv[1], argv[1]);
+  sprintf(link_cmd, link_cmd_pattern, argv[1], BFSTD, argv[1]);
 
   printf("Building... ");
 
-  FILE* build_cmd_pipe = popen(compile_cmd, "r");
-  if ( build_cmd_pipe == NULL) {
+  FILE *build_cmd_pipe = popen(compile_cmd, "r");
+  if (build_cmd_pipe == NULL) {
     printf("fail\nError opening pipe for building\n");
     free(compile_cmd);
     free(link_cmd);
@@ -246,8 +253,7 @@ int main(const int argc, char **argv)
   }
 
   int *build_result = malloc(sizeof(int));
-  if ((*build_result = pclose(build_cmd_pipe)) != 0)
-  {
+  if ((*build_result = pclose(build_cmd_pipe)) != 0) {
     printf("fail\nBuild failed with the exit code %d", *build_result);
     free(compile_cmd);
     free(link_cmd);
@@ -261,9 +267,8 @@ int main(const int argc, char **argv)
 
   printf("Linking... ");
 
-  FILE* link_cmd_pipe = popen(link_cmd, "r");
-  if ( link_cmd_pipe == NULL)
-  {
+  FILE *link_cmd_pipe = popen(link_cmd, "r");
+  if (link_cmd_pipe == NULL) {
     printf("fail\nError opening pipe for linking\n");
     free(link_cmd);
     free(output_file);
