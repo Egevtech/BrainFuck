0e352fe15ab7e6f1e810c2152c89e3ea951272c0
egevtech <egevtech@gmail.com>
2026-03-02 02:39:45 +0300

    Add run mode and returned CMake build system

--- src/main.c
+++ src/main.c
@@ -1,16 +1,26 @@
 #define _POSIX_C_SOURCE 200809L
 
+/*
+ * Важное замечание от автора
+ * Сейчас программа работает нестабильно, непонятно и т.п.
+ * Проект будет заброшен как минимум на эту неделю, потому вот так вот.
+ * Починю потом, наверное
+ *
+ * не поминайте лихом
+ */
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "vec.h"
 
-#define INPUT_FILENAME_LEN sizeof(argv[1])
+#define INPUT_FILENAME_LEN strlen(output_file_arg)
 
 #ifndef BFSTD
-#define BFSTD "./libbfstd.a"
+#define BFSTD "build/libbfstd.a"
 #endif
 
 #define REG_TOKEN(symbol, token_name) \
@@ -45,13 +55,27 @@ Token *parse_token(int sym) {
 int main(const int argc, char **argv) {
   printf("BFC - brainfuck compiler\n");
 
-  if (argc != 2) {
-    printf("USAGE: bfc FILE\n"
-           "USAGE: FILE - file to compile\n");
+  char* output_file_arg = argv[2];
+
+  if (argc != 3)
+  {
+    printf("USAGE: bfc [MODE] [FILE]\n"
+            "[MODE] = {run, build}\n");
     return EXIT_FAILURE;
   }
 
-  FILE *file = fopen(argv[1], "r");
+  int need_run = 0;
+
+  if (strcmp(argv[1], "run") == 0) {
+      need_run = 1;
+  } else if (strcmp(argv[1], "build") != 0)
+  {
+    // TODO: я переделаю это потом и сделаю нормальный обработчик
+    printf("Invalid usage. Available modes are: run, build\n");
+    return EXIT_FAILURE;
+  }
+
+  FILE *file = fopen(output_file_arg, "r");
 
   if (file == NULL) {
     printf("Open file failed\n");
@@ -81,15 +105,15 @@ int main(const int argc, char **argv) {
 
   fclose(file);
 
-  void *output_file = malloc(sizeof(char) * (strlen(argv[1]) + 2));
-  strcpy(output_file, argv[1]);
+  char *output_file = malloc(sizeof(char) * (strlen(output_file_arg) + 2));
+  strcpy(output_file, output_file_arg);
   strcat(output_file, ".s");
 
   FILE *output = fopen(output_file, "w");
+  free(output_file);
 
   if (output == NULL) {
     printf("Can't open output file\n");
-    free(output_file);
     return EXIT_FAILURE;
   }
 
@@ -106,7 +130,7 @@ int main(const int argc, char **argv) {
   char *operation = malloc(20 * sizeof(char));
 
   for (int i = 0; i < tokens.len; i++) {
-    int pr = ((float)i + 1) / tokens.len * 100.0f;
+    int pr = ((float)i + 1.00f) / tokens.len * 100.0f;
 
     printf("Compiling... %d%%\r", pr);
 
@@ -116,7 +140,7 @@ int main(const int argc, char **argv) {
         break
 
     switch (tokens.data[i]) {
-      TOKEN2CALL(PLUS, add_cell); 
+      TOKEN2CALL(PLUS, add_cell);
       TOKEN2CALL(MINUS, sub_cell);
       TOKEN2CALL(NEXT, next_cell);
       TOKEN2CALL(PREV, prev_cell);
@@ -140,12 +164,12 @@ int main(const int argc, char **argv) {
   printf("Compiling... done\n");
 
   const char *compile_cmd_pattern = "nasm -felf64 %s.s -o %s.o";
-  const char *link_cmd_pattern = "ld %s.o %s -o a.out -lc "
+  const char *link_cmd_pattern = "ld %s.o %s -o %s.out -lc "
                                  "-dynamic-linker /lib64/ld-linux-x86-64.so.2";
 
-  char *compile_cmd = malloc(strlen(compile_cmd_pattern) + strlen(argv[1]) * 2 + 1);
-  char *link_cmd = malloc(strlen(link_cmd_pattern) + 
-    strlen(argv[1]) * 2 + 1 + strlen(BFSTD));
+  char *compile_cmd = malloc(strlen(compile_cmd_pattern) + strlen(output_file_arg) * 2 + 1);
+  char *link_cmd = malloc(strlen(link_cmd_pattern) +
+    strlen(output_file_arg) * 2 + 5 + strlen(BFSTD));
 
   FILE* stdlib_file = fopen(BFSTD, "r");
   char* fstdlib_name = malloc(sizeof(char) * 30);
@@ -158,8 +182,8 @@ int main(const int argc, char **argv) {
     fclose(stdlib_file);
   }
 
-  sprintf(compile_cmd, compile_cmd_pattern, argv[1], argv[1]);
-  sprintf(link_cmd, link_cmd_pattern, argv[1], fstdlib_name, argv[1]);
+  sprintf(compile_cmd, compile_cmd_pattern, output_file_arg, output_file_arg);
+  sprintf(link_cmd, link_cmd_pattern, output_file_arg, fstdlib_name, output_file_arg);
 
   free(fstdlib_name);
 
@@ -170,7 +194,6 @@ int main(const int argc, char **argv) {
     printf("fail\nError opening pipe for building\n");
     free(compile_cmd);
     free(link_cmd);
-    free(output_file);
     return EXIT_FAILURE;
   }
 
@@ -180,7 +203,6 @@ int main(const int argc, char **argv) {
     free(compile_cmd);
     free(link_cmd);
     free(build_result);
-    free(output_file);
     return EXIT_FAILURE;
   }
 
@@ -212,7 +234,6 @@ int main(const int argc, char **argv) {
 
   free(link_cmd);
   free(link_result);
-  free(output_file);
 
   printf("Cleaning... ");
 
@@ -228,20 +249,47 @@ int main(const int argc, char **argv) {
     return EXIT_FAILURE;
   }
 
-  sprintf(nasm_file, "./%s.s", argv[1]);
-  sprintf(object_file, "./%s.o", argv[1]);
+  sprintf(nasm_file, "%s.s", output_file_arg);
+  sprintf(object_file, "%s.o", output_file_arg);
 
   if (remove(nasm_file) != 0 || remove(object_file) != 0) {
     free(nasm_file);
     free(object_file);
     printf("fail\n");
     perror("Cleaning failed");
+    return EXIT_FAILURE;
   }
 
   free(nasm_file);
   free(object_file);
 
   printf("done\n");
-  
+
+  if (need_run != 1) {
+      return EXIT_SUCCESS;
+  }
+
+  printf("Preparing for run...");
+
+  char* output_filename = malloc(sizeof(char) * INPUT_FILENAME_LEN + 4);
+
+  sprintf(output_filename, "%s.out", output_file_arg);
+
+
+  if (access(output_filename, F_OK)) {
+      printf(" fail\nCan't find executable file %s\n", output_filename);
+
+      free(output_filename);
+      return EXIT_FAILURE;
+  }
+
+  printf("done\n");
+
+  printf("Running %s\n", output_filename);
+
+  execv(output_filename, NULL); //TODO: Link file into output_file_arg.out across a.out
+
+  free(output_filename);
+
   return EXIT_SUCCESS;
 }
